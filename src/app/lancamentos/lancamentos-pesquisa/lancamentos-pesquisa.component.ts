3b1168b579278d5608a9c8a6c03b180951afeef0
danieldelajustina <dd.justina@hotmail.com>
2019-05-13 18:58:26 -0300

    mudar paginas

--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { LancamentoService, LancamentoFiltro } from '../lancamento.service';
-
+import { LazyLoadEvent } from 'primeng/components/common/api';
 
 @Component({
   selector: 'app-lancamentos-pesquisa',
   templateUrl: './lancamentos-pesquisa.component.html',
   styleUrls: ['./lancamentos-pesquisa.component.css']
 })
-export class LancamentosPesquisaComponent  implements OnInit {
+export class LancamentosPesquisaComponent {
 
   lancamentos = [];
   total: number;
@@ -15,16 +15,18 @@ export class LancamentosPesquisaComponent  implements OnInit {
 
   constructor(private lancamentoService: LancamentoService) {  }
 
-  ngOnInit() {
-    this.pesquisar();
-  }
-
-  pesquisar() {
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
     this.lancamentoService.pesquisar(this.filtro)
       .then((data) => {
         this.lancamentos = data.content;
         this.total = data.totalElements;
       }
     );
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
   }
 }
